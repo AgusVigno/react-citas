1d0fbb09b813141b295f2420c63fea9cd1191aa3
Agustin Vignolo <agustinvignolo@MacBook-Air-de-Agustin.local>
2020-05-29 21:54:38 -0300

    Updates

---
+++ precache-manifest.eb4502c69fc813c8a460ed847cb4cbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0897f0a69911b9564f81816fd7ca72",
+    "url": "/react-citas/index.html"
+  },
+  {
+    "revision": "e99e98d792edf04aee28",
+    "url": "/react-citas/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "9dfa0786d5e60bddaf97",
+    "url": "/react-citas/static/js/2.f89b7caf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-citas/static/js/2.f89b7caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99e98d792edf04aee28",
+    "url": "/react-citas/static/js/main.94fb5cdb.chunk.js"
+  },
+  {
+    "revision": "00dadf8e8d2f6dde62cd",
+    "url": "/react-citas/static/js/runtime-main.ea3113d4.js"
+  }
+]);
